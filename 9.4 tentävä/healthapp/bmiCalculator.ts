69f9cac357ae6c1d7ad446f9a98407ba3c975d19
onni <holttaonni@gmail.com>
2026-09-24 13:46:51 +0300

    Add files via upload

---
+++ 9.4 tentävä/healthapp/bmiCalculator.ts
@@ -0,0 +1,26 @@
+const calculateBmi = (height: number, weight: number): string => {
+    const bmi = weight / ((height / 100) ** 2);
+
+    if (bmi < 18.5) {
+        return 'Underweight';
+    }
+
+    if (bmi < 25) {
+        return 'Normal range';
+    }
+
+    if (bmi < 30) {
+        return 'Overweight';
+    }
+
+    return 'Obese';
+};
+
+const height = Number(process.argv[2]);
+const weight = Number(process.argv[3]);
+
+if (Number.isNaN(height) || Number.isNaN(weight)) {
+    console.log('malformatted parameters');
+} else {
+    console.log(calculateBmi(height, weight));
+}
