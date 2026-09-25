a804745bc60643af41248c9f10c2113e8a598e1b
onni <holttaonni@gmail.com>
2026-09-25 12:17:22 +0300

    Change POST /bmi endpoint to GET /bmi

--- 9.6 tentävä/healthapp/index.ts
+++ 9.6 tentävä/healthapp/index.ts
@@ -7,7 +7,7 @@ app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
 
-app.post('/bmi', (req, res) => {
+app.get('/bmi', (req, res) => {
   const height = Number(req.query.height);
   const weight = Number(req.query.weight);
 
