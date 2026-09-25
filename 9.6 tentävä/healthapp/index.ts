517c174226ded0700e75b782a9fa578e1e8f0644
onni <holttaonni@gmail.com>
2026-09-25 12:15:21 +0300

    Update index.ts

--- 9.6 tentävä/healthapp/index.ts
+++ 9.6 tentävä/healthapp/index.ts
@@ -7,7 +7,7 @@ app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
 
-app.get('/bmi', (req, res) => {
+app.post('/bmi', (req, res) => {
   const height = Number(req.query.height);
   const weight = Number(req.query.weight);
 
